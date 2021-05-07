9e8ffc8b5e1221f43c342c13eb810f21aff91104
NORTHAMERICA\selliott <selliott@microsoft.com>
2021-05-07 10:29:16 -0400

    Add PublicIP (temp) + Install Jenkins

--- caf_solution/add-ons/jenkins/jenkins.tf
+++ caf_solution/add-ons/jenkins/jenkins.tf
@@ -21,6 +21,20 @@ resource "azurerm_virtual_network" "jennetwork" {
     }
 }
 
+resource "azurerm_public_ip" "jnPubIP" {
+    depends_on = [azurerm_resource_group.jenrg]
+    for_each   = try(var.jenkins_rg, {})
+  name                = "jnPubIP"
+  location            = azurerm_resource_group.jenrg[each.key].location
+  resource_group_name = azurerm_resource_group.jenrg[each.key].name
+  allocation_method = "Static"
+  idle_timeout_in_minutes      = 30
+
+  tags = {
+    environment = "shawn"
+  }
+}
+
 resource "azurerm_subnet" "jensubnet" {
     depends_on = [azurerm_resource_group.jenrg, azurerm_virtual_network.jennetwork]
     for_each   = try(var.jenkins_rg, {})
@@ -31,7 +45,7 @@ resource "azurerm_subnet" "jensubnet" {
 }
 
 resource "azurerm_network_interface" "jennic" {
-      depends_on = [azurerm_resource_group.jenrg]
+    depends_on = [azurerm_resource_group.jenrg]
     for_each   = try(var.jenkins_rg, {})
   name                = "jennic"
   location            = azurerm_resource_group.jenrg[each.key].location
@@ -41,8 +55,17 @@ resource "azurerm_network_interface" "jennic" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.jensubnet[each.key].id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.jnPubIP[each.key].id
   }
 }
+
+locals {
+    jdk = "openjdk-8-jdk"
+    aptPrefix = "sudo DEBIAN_FRONTEND=noninteractive apt --yes -qq"
+    aptUpdate = "${local.aptPrefix} update "
+    aptInstall = "${local.aptPrefix} install "
+}
+
 resource "azurerm_linux_virtual_machine" "jenvm" {
       depends_on = [azurerm_resource_group.jenrg]
     for_each   = try(var.jenkins_rg, {})
@@ -70,5 +93,28 @@ resource "azurerm_linux_virtual_machine" "jenvm" {
     offer     = "UbuntuServer"
     sku       = "16.04-LTS"
     version   = "latest"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      # general update
+      "echo '===[01]==='; ${local.aptUpdate}",
+      # install Java SDK
+      "echo '===[02]==='; ${local.aptInstall} ${local.jdk}",
+      # Install Jenkins
+      "echo '===[03]==='; wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -",
+      "echo '===[04]==='; sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'",
+      "echo '===[05]==='; ${local.aptUpdate}",
+      "echo '===[06]==='; ${local.aptInstall} jenkins",
+      # Start Jenkins service
+      "echo '===[07]==='; sudo systemctl start jenkins",
+      "echo '===[08]===';",
+    ]
+    connection {
+      type = "ssh"
+      user = "adminuser"
+      private_key = file("~/.ssh/SENexis_key.pem")
+      host = azurerm_public_ip.jnPubIP[each.key].ip_address
+    }
   }
 }
